08289dea60369b0eda1ea7f42c60fb39f5b72b31
OnjoujiToki <z@moon.moe>
2022-12-02 23:38:34 -0800

    update test for fenwick tree

--- data structure/fenwick.cpp
+++ data structure/fenwick.cpp
@@ -3,14 +3,17 @@ struct FenwickTree {
   std::vector<T> bit;
   int n;
   FenwickTree(int _n) : n(_n), bit(_n) {}
-  // 0 based
+  FenwickTree(std::vector<T> a) {
+    bit.resize(n, 0);
+    for (int i = 0; i < (int)a.size(); i++) add(i, (int)a[i]);
+  }
   T sum(int r) {
     int ret = 0;
     for (; r >= 0; r = (r & (r + 1)) - 1) ret += bit[r];
     return ret;
   }
-  // try commits:::
-  T sum(int l, int r) { return sum(r) - sum(l - 1); }
+
+  T sum(int l, int r) { return sum(r) - sum(l - 1); }  // [l, r]
 
   void add(int idx, int delta) {
     for (; idx < n; idx = idx | (idx + 1)) bit[idx] += delta;
